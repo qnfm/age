62d5e786ef2876a4be47fb5cae689374dbe557f1
qnfm <uzinag@163.com>
2022-04-26 16:47:52 +0800

    Remove all X25519 in comments. Upgrade to Kyber1024

--- kyber1024.go
+++ kyber1024.go
@@ -14,25 +14,25 @@ import (
 
 	"filippo.io/age/internal/bech32"
 	"filippo.io/age/internal/format"
-	"github.com/cloudflare/circl/pke/kyber/kyber512"
+	"github.com/cloudflare/circl/pke/kyber/kyber1024"
 	"golang.org/x/crypto/sha3"
 )
 
-const KyberLabel = "age-encryption.org/v3/Kyber512"
+const Kyber1024Label = "age-encryption.org/v3/Kyber1024"
 
-// X25519Recipient is the standard age public key. Messages encrypted to this
-// recipient can be decrypted with the corresponding X25519Identity.
+// Kyber1024Recipient is the standard age public key. Messages encrypted to this
+// recipient can be decrypted with the corresponding Kyber1024Identity.
 //
 // This recipient is anonymous, in the sense that an attacker can't tell from
 // the message alone if it is encrypted to a certain recipient.
-type Kyber512Recipient struct {
+type Kyber1024Recipient struct {
 	theirPublicKey []byte
 }
 
-var _ Recipient = &Kyber512Recipient{}
+var _ Recipient = &Kyber1024Recipient{}
 
-// ParseX25519Recipient returns a new Kyber512Recipient from a raw string without any encoding
-func ParseKyber512Recipient(s string) (*Kyber512Recipient, error) {
+// ParseKyber1024Recipient returns a new Kyber1024Recipient from a raw string without any encoding
+func ParseKyber1024Recipient(s string) (*Kyber1024Recipient, error) {
 	t, k, err := bech32.Decode(s)
 	if err != nil {
 		return nil, fmt.Errorf("malformed recipient %q: %v", s, err)
@@ -41,20 +41,20 @@ func ParseKyber512Recipient(s string) (*Kyber512Recipient, error) {
 		return nil, fmt.Errorf("malformed recipient %q: invalid type %q", s, t)
 	}
 
-	return &Kyber512Recipient{theirPublicKey: k}, nil
+	return &Kyber1024Recipient{theirPublicKey: k}, nil
 }
 
-func (r *Kyber512Recipient) Wrap(fileKey []byte) ([]*Stanza, error) {
+func (r *Kyber1024Recipient) Wrap(fileKey []byte) ([]*Stanza, error) {
 
 	h := sha3.NewShake256()
-	ct := make([]byte, kyber512.CiphertextSize)
+	ct := make([]byte, kyber1024.CiphertextSize)
 	h.Reset()
-	//ct := publickey.Encrypt(pt:e,seed:H(filekey||pk))
+	//ct := publickey.Encrypt(pt:filekey,seed:H(filekey||pk))
 	h.Write(fileKey)
 	h.Write(r.theirPublicKey)
-	seed := make([]byte, kyber512.KeySeedSize)
+	seed := make([]byte, kyber1024.KeySeedSize)
 	h.Read(seed[:])
-	var p kyber512.PublicKey
+	var p kyber1024.PublicKey
 	p.Unpack(r.theirPublicKey)
 
 	p.EncryptTo(ct, fileKey[:], seed[:])
@@ -67,7 +67,7 @@ func (r *Kyber512Recipient) Wrap(fileKey []byte) ([]*Stanza, error) {
 	h.Read(sig[:])
 
 	l := &Stanza{
-		Type: "Kyber512",
+		Type: "Kyber1024",
 		Args: []string{format.EncodeToString(sig)},
 		Body: ct,
 	}
@@ -76,23 +76,23 @@ func (r *Kyber512Recipient) Wrap(fileKey []byte) ([]*Stanza, error) {
 }
 
 // String returns the Bech32 public key encoding of r.
-func (r *Kyber512Recipient) String() string {
+func (r *Kyber1024Recipient) String() string {
 	s, _ := bech32.Encode("age", r.theirPublicKey)
 	return s
 }
 
-// Kyber512Identity is the custom age private key, which can decrypt messages
-// encrypted to the corresponding Kyber512Recipient.
-type Kyber512Identity struct {
+// Kyber1024Identity is the custom age private key, which can decrypt messages
+// encrypted to the corresponding Kyber1024Recipient.
+type Kyber1024Identity struct {
 	ks []byte
 }
 
-var _ Identity = &Kyber512Identity{}
+var _ Identity = &Kyber1024Identity{}
 
-// GenerateKyber512Identity randomly generates a new Kyber512Identity.
-func GenerateKyber512Identity() (*Kyber512Identity, error) {
-	var i Kyber512Identity
-	var seed [kyber512.KeySeedSize]byte
+// GenerateKyber1024Identity randomly generates a new Kyber1024Identity.
+func GenerateKyber1024Identity() (*Kyber1024Identity, error) {
+	var i Kyber1024Identity
+	var seed [kyber1024.KeySeedSize]byte
 	_, err := io.ReadFull(rand.Reader, seed[:])
 	if err != nil {
 		return nil, err
@@ -104,9 +104,9 @@ func GenerateKyber512Identity() (*Kyber512Identity, error) {
 	return &i, err
 }
 
-// ParseKyber512Identity returns a new Kyber512Identity from a Kyber512 private key
+// ParseKyber1024Identity returns a new Kyber1024Identity from a Kyber1024 private key
 // encoding with the "AGE-SECRET-KEY-1" prefix.
-func ParseKyber512Identity(s string) (*Kyber512Identity, error) {
+func ParseKyber1024Identity(s string) (*Kyber1024Identity, error) {
 	t, k, err := bech32.Decode(s)
 	if err != nil {
 		return nil, fmt.Errorf("malformed secret key: %v", err)
@@ -115,31 +115,31 @@ func ParseKyber512Identity(s string) (*Kyber512Identity, error) {
 		return nil, fmt.Errorf("malformed secret key: unknown type %q", t)
 	}
 
-	return &Kyber512Identity{ks: k}, nil
+	return &Kyber1024Identity{ks: k}, nil
 }
 
-func (i *Kyber512Identity) Unwrap(stanzas []*Stanza) ([]byte, error) {
+func (i *Kyber1024Identity) Unwrap(stanzas []*Stanza) ([]byte, error) {
 	return multiUnwrap(i.unwrap, stanzas)
 }
 
-func (i *Kyber512Identity) unwrap(block *Stanza) ([]byte, error) {
-	if block.Type != "Kyber512" {
+func (i *Kyber1024Identity) unwrap(block *Stanza) ([]byte, error) {
+	if block.Type != "Kyber1024" {
 		return nil, ErrIncorrectIdentity
 	}
 	if len(block.Args) != 1 {
-		return nil, errors.New("invalid Kyber512 recipient block")
+		return nil, errors.New("invalid Kyber1024 recipient block")
 	}
 	sig, err := format.DecodeString(block.Args[0])
 	if err != nil {
-		return nil, fmt.Errorf("failed to parse Kyber512 sig: %v", err)
+		return nil, fmt.Errorf("failed to parse Kyber1024 sig: %v", err)
 	}
 	if len(sig[:]) != 32 {
 		fmt.Println(len(sig))
-		return nil, errors.New("invalid Kyber512 encap sig size")
+		return nil, errors.New("invalid Kyber1024 encap sig size")
 	}
 
-	fileKey := make([]byte, kyber512.PlaintextSize)
-	_, sk := kyber512.NewKeyFromSeed(i.ks[:])
+	fileKey := make([]byte, kyber1024.PlaintextSize)
+	_, sk := kyber1024.NewKeyFromSeed(i.ks[:])
 	sk.DecryptTo(fileKey, block.Body)
 	h := sha3.NewShake256()
 	//signature := H(filekey||ct)
@@ -148,24 +148,24 @@ func (i *Kyber512Identity) unwrap(block *Stanza) ([]byte, error) {
 	sig2 := make([]byte, 32)
 	h.Read(sig2[:])
 	if subtle.ConstantTimeCompare(sig[:], sig2[:]) == 0 {
-		return nil, fmt.Errorf("invalid Kyber512 encap sig")
+		return nil, fmt.Errorf("invalid Kyber1024 encap sig")
 	}
 
 	return fileKey, nil
 }
 
-// Recipient returns the public Kyber512Recipient value corresponding to i.
-func (i *Kyber512Identity) Recipient() *Kyber512Recipient {
-	var r Kyber512Recipient
-	pk, _ := kyber512.NewKeyFromSeed(i.ks)
-	buf := make([]byte, kyber512.PublicKeySize)
+// Recipient returns the public Kyber1024Recipient value corresponding to i.
+func (i *Kyber1024Identity) Recipient() *Kyber1024Recipient {
+	var r Kyber1024Recipient
+	pk, _ := kyber1024.NewKeyFromSeed(i.ks)
+	buf := make([]byte, kyber1024.PublicKeySize)
 	pk.Pack(buf)
 	r.theirPublicKey = buf
 	return &r
 }
 
 // String returns the seed of private key
-func (i *Kyber512Identity) String() string {
+func (i *Kyber1024Identity) String() string {
 	s, _ := bech32.Encode("AGE-SECRET-KEY-", i.ks)
 	return strings.ToUpper(s)
 }
